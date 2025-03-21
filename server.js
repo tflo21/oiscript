e5cec92834377e3782aab3f24864e46c5aed618c
tflo21 <85530562+tflo21@users.noreply.github.com>
2025-03-21 15:54:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
+require("./cronJob"); // Auto-starts the cron job
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
